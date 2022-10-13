1e64edfd3ac66bcb3c26e51791809379cea1e11c
YESOL PARK <47820809+yesoly@users.noreply.github.com>
2022-10-13 18:36:37 +0900

    update

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.views.decorators import gzip
 from django.http import StreamingHttpResponse
 import cv2
 import threading
+from playsound import playsound
 from django.contrib.auth import login as auth_login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
@@ -18,37 +19,51 @@ product_price = [3000, 1000, 3500, 1500, 4000, 3000, 2000, 3200, 3800, 4500, 480
 total_price = 0
 products = []
 object_list = []
+page = 0
+signal = False
+
 
 def main(request):
     global page
+    global signal
+
     if request.method == 'POST': # 지금은 버튼 이벤트로 1을 받아오도록 구현
-        page =  int(request.POST.get("move_page"))
+        page = int(request.POST.get("move_page"))
         print(page)
         ######## YES를 대답해서 page 값 1을 받아오면 ########
         # TTS) 벨트 위에 물건을 차례차례 올려주세요.
         # 컨베이어 벨트 작동
-    else:
-        page = 0 # default = 0
-        
+
+    if signal == True:
+        page = subscribe_tester.page
+        signal = False
+        return render(request, 'main/main.html', {'products': products, 'total': total_price, 'page': page})
     # page = 0: 시작페이지 (default)
     # page = 1: detectron 페이지
     # page = 2: 총액 안내 및 로봇 액션 페이지
     # page = 3: Good bye 페이지
     return render(request, 'main/main.html', {'products': products, 'total': total_price, 'page': page})
 
-
 class SubscribeTester:
     def __init__(self):
         self.cv_bridge = CvBridge()
         detectron_topic = '/detectron_img'
         object_label_topic = '/object_labels'
+        page_num_topic = '/page_num'
         _image_sub = rospy.Subscriber(detectron_topic, Image, self._rgb_callback)
         _image_sub2 = rospy.Subscriber(object_label_topic, Int32MultiArray, self._object_label_callback)
+        _page_num_sub = rospy.Subscriber(page_num_topic, Int16, self._page_num_callback)
 
     def _rgb_callback(self, img_msg):
         self.cv_image = self.cv_bridge.imgmsg_to_cv2(img_msg, 'passthrough')
-        #cv2.imshow('detecron_img', self.cv_image)
-        #cv2.waitKey(1)
+        # cv2.imshow('detecron_img', self.cv_image)
+        # cv2.waitKey(1)
+
+    # @api_view(['GET', 'POST', ])
+    def _page_num_callback(self, data):
+        global signal
+        self.page = int(data.data)
+        print('page', self.page)
 
     def _object_label_callback(self, data):
         self.object_label_list = list(data.data)
@@ -64,29 +79,45 @@ class SubscribeTester:
             if i not in object_list:
                 object_list.append(i)
                 products.append([product_name[i], 1, product_price[i], product_price[i]])
-                
-                ###### Todo) detect 효과음 추가 ######
-
+                playsound("/home/snubi/PycharmProjects/snubi_zeus2022/zeusweb-master/beep.mp3")
                 global total_price
                 total_price += product_price[i]
 
-                ######## if i == 계란 index: #########
-                # TTS) 깨지기 쉬운 계란은 나중에 넣을게요!
-                # 계란 나중에 넣는 로봇 Task 수행
-                ######################################
-
                 print(products)
+
+
 class VideoCamera(object):
     def __init__(self):
         self.frame = subscribe_tester.cv_image
         threading.Thread(target=self.update, args=()).start()
+
     def get_frame(self):
         image = self.frame
         _, jpeg = cv2.imencode('.jpg', image)
         return jpeg.tobytes()
+
     def update(self):
         while True:
             self.frame = subscribe_tester.cv_image
+
+class FaceVideoCamera(object):
+    def __init__(self):
+        # Facedetection video
+        self.video = cv2.VideoCapture(0)
+        (self.grabbed, self.frame) = self.video.read()
+        threading.Thread(target=self.update, args=()).start()
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        image = self.frame
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
+
+    def update(self):
+        while True:
+            (self.grabbed, self.frame) = self.video.read()
 
 
 def gen(camera):
@@ -104,5 +135,14 @@ def camera(request):
         print("에러입니다...")
         pass
 
-rospy.init_node('ui_image_ros_subscriber')
+@gzip.gzip_page
+def face(request):
+    try:
+        facecam = FaceVideoCamera()
+        return StreamingHttpResponse(gen(facecam), content_type="multipart/x-mixed-replace;boundary=frame")
+    except:  # This is bad! replace it with proper handling
+        print("에러입니다...")
+        pass
+
 subscribe_tester = SubscribeTester()
+rospy.init_node('ui_image_ros_subscriber')
